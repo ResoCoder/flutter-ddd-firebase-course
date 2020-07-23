f02e8a20996a3993f6f19bc3ebf455fa2873ac9c
Matt <mattresetar@resocoder.com>
2020-07-23 20:56:45 +0200

    22 - Uncompleted Switch

--- lib/presentation/notes/notes_overview/notes_overview_page.dart
+++ lib/presentation/notes/notes_overview/notes_overview_page.dart
@@ -7,6 +7,7 @@ import 'package:notes_firebase_ddd_course/application/notes/note_actor/note_acto
 import 'package:notes_firebase_ddd_course/application/notes/note_watcher/note_watcher_bloc.dart';
 import 'package:notes_firebase_ddd_course/injection.dart';
 import 'package:notes_firebase_ddd_course/presentation/notes/notes_overview/widgets/notes_overview_body_widget.dart';
+import 'package:notes_firebase_ddd_course/presentation/notes/notes_overview/widgets/uncompleted_switch.dart';
 import 'package:notes_firebase_ddd_course/presentation/routes/router.gr.dart';
 
 class NotesOverviewPage extends StatelessWidget {
@@ -63,10 +64,7 @@ class NotesOverviewPage extends StatelessWidget {
               },
             ),
             actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.indeterminate_check_box),
-                onPressed: () {},
-              )
+              UncompletedSwitch(),
             ],
           ),
           body: NotesOverviewBody(),
---
+++ lib/presentation/notes/notes_overview/widgets/uncompleted_switch.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:notes_firebase_ddd_course/application/notes/note_watcher/note_watcher_bloc.dart';
+
+class UncompletedSwitch extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final toggleState = useState(false);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: InkResponse(
+        onTap: () {
+          toggleState.value = !toggleState.value;
+          context.bloc<NoteWatcherBloc>().add(
+                toggleState.value
+                    ? const NoteWatcherEvent.watchUncompletedStarted()
+                    : const NoteWatcherEvent.watchAllStarted(),
+              );
+        },
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 100),
+          transitionBuilder: (child, animation) => ScaleTransition(
+            scale: animation,
+            child: child,
+          ),
+          child: toggleState.value
+              ? Icon(
+                  Icons.check_box_outline_blank,
+                  key: const Key('outline'),
+                )
+              : Icon(
+                  Icons.indeterminate_check_box,
+                  key: const Key('indeterminate'),
+                ),
+        ),
+      ),
+    );
+  }
+}
